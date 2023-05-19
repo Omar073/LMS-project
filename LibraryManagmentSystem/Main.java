77d9c2126ddb4bc18c034beeb316f00b8ceb63dd
Omar073 <omarahmed7703@gmail.com>
2023-05-19 11:46:42 +0300

    edit

--- LibraryManagmentSystem/Main.java
+++ LibraryManagmentSystem/Main.java
@@ -1,8 +1,6 @@
 public class Main {
   public static void main(String[] args) throws Exception {
 
-    System.out.println("                 Welcome to the Library Management System!");
-
     Library library = new Library();
 
     MyHelloApp.main(args); //    GUI  
